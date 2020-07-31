04d5d0aa5f280bef305daf52f795df43101dad64
conig <james.conigrave@gmail.com>
2020-07-31 12:09:18 +1000

    add elipses

--- R/plot_reg.r
+++ R/plot_reg.r
@@ -3,35 +3,42 @@
 #' function
 #' @param lm a linear model
 #' @param x character, predictor
+#' @param ... extra arguments passed to plot
 #' @export
 
-plot_reg <- function(lm, x = NULL) {
-function_call = match.call()
-form = as.formula(lm$call$formula)
-outcome = as.character(form[2])
+plot_reg <- function(lm, x = NULL, ...) {
+  function_call = match.call()
+  form = as.formula(lm$call$formula)
+  outcome = as.character(form[2])
+  args <- list(...)
 
-if (is.null(x)) {
+  if (is.null(x)) {
+    split = function(x, split) trimws(strsplit(x, split = split)[[1]][1])
 
-  split = function(x, split) trimws(strsplit(x, split = split)[[1]][1])
+    pred = split(as.character(form[3]), split = "\\+")
+    pred =  split(pred, "\\*")
+    pred =  split(pred, "\\:")
+    pred =  split(pred, "\\-")
 
-  pred = split(as.character(form[3]), split = "\\+")
-  pred =  split(pred, "\\*")
-  pred =  split(pred, "\\:")
+  } else{
+    pred = function_call$x
+  }
 
-} else{
-  pred = function_call$x
-}
+  call  <-
+    list(formula = as.formula(glue::glue("{outcome} ~ {pred}")),
+         data = lm$call$data)
+  if(length(args) > 0){
+  call = append(call, args)
+  }
 
-call  <- list(formula = as.formula(glue::glue("{outcome} ~ {pred}")),
-              data = lm$call$data)
+  coefs = coef(lm)
 
-coefs = coef(lm)
+  intercept <- coefs[grepl("intercept", names(coefs), ignore.case = T)]
+  intercept <- ifelse(length(intercept) == 0, 0 , intercept)
+  slope = coefs[names(coefs) == pred]
 
-intercept <- coefs[[1]]
-slope = coefs[names(coefs) == pred]
+  do.call(plot, call)
 
-do.call(plot, call)
-
-abline(a = intercept, b = slope)
+  abline(a = intercept, b = slope)
 
 }
