a9be57c7c5f280c026b8b0582e009591c1b3ec57
conig <james.conigrave@gmail.com>
2020-08-12 17:08:52 +1000

    simplify arguments

--- R/bibtex.R
+++ R/bibtex.R
@@ -1,12 +1,11 @@
 #' open_citation
 #'
 #' Write a bibtex citation to a temporary file and open it. This may assist with loading R references into Zotero
-#' @param citation citation object to open with zotero
-#' @param pacakge the package name
+#' @param package the package name
 #' @export
 
-open_citation = function(citation = NULL, package = NULL){
-  if(!is.null(package)) citation <- citation(package)
+open_citation = function(package = NULL){
+  citation <- citation(package)
   bib <- utils::toBibtex(citation)
   path <- tempfile(fileext = ".bib")
   write(bib, path)
